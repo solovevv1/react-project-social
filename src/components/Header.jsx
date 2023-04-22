f98fdb118785ee4fd094143dfc7b4ce816f2070c
solovevv1 <solovevvs1@gmail.com>
2023-04-22 19:48:38 +0530

    followed 1-13

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://i.pinimg.com/originals/65/63/64/656364729a56be6f0986d8a9e69749c7.png" />
+    </header>
+}
+
+export default Header;
