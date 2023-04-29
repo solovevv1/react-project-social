84254e7e088ee1d7fdbe04636fd7ade39fd2e10c
solovevv1 <solovevvs1@gmail.com>
2023-04-29 20:10:17 +0530

    dialogs page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import './App.css';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
+import Dialogs from './components/Dialogs/Dialogs';
+import Header  from './components/Header/Header';
+import Navbar  from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 
 
@@ -10,7 +11,10 @@ const App = () => {
     <div className='app-wrapper'>
       <Header />
       <Navbar />
-      <Profile />
+      {/* <Profile /> */}
+      <div className='app-wrapper-content'>
+      <Dialogs />
+      </div>
     </div>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className={s.content}>
+    return <div>
         <img src='https://cleanlife.wdfiles.com/local--files/postcover:beach-header-2/beach-header.jpg' />
         <div>
             ava + description
