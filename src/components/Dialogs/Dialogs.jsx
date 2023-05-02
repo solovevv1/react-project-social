2eb56e8a1c7919031c3053cbae747ca47d2e9c40
solovevv1 <solovevvs1@gmail.com>
2023-05-02 22:05:52 +0530

    adding dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/state';
 import DialogItem from './DialogItem/DialogItem';
 import s from './Dialogs.module.css';
 import Message from './Message/Message';
@@ -13,6 +14,16 @@ const Dialogs = (props) => {
     let messageElements = props.state.messagesData.map((messages) => {
         return <Message message={messages.message} />
     });
+    let newMessageBody = props.state.newMessageBody;
+
+
+    let onSendMessageClick = () => {
+        props.store.dispatch(sendMessageCreator());
+    };
+    let onNewMessageChange = (event) => {
+        let body = event.target.value;
+        props.store.dispatch(updateNewMessageBodyCreator(body));
+    };
 
     return (
         <div className={s.dialogs}>
@@ -20,15 +31,19 @@ const Dialogs = (props) => {
                 {dialogElements}
                 {/* <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
                 <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} /> */}
+                ... */}
             </div>
             <div className={s.messages}>
-                {messageElements}
+                <div>{messageElements}</div>
+                <div>
+                    <div><textarea value={newMessageBody}
+                                   onChange={onNewMessageChange}
+                                   placeholder='Enter your message'></textarea></div>
+                    <div><button onClick={ onSendMessageClick }>Send</button></div>
+                </div>
                 {/* <Message message={messagesData[0].message} />
                 <Message message={messagesData[1].message} />
-                <Message message={messagesData[2].message} /> */}
+                ... */}
             </div>
         </div>
     )
--- src/redux/state.js
+++ src/redux/state.js
@@ -19,7 +19,9 @@ let store = {
             messagesData: [
                 { id: 1, message: 'Hey!' },
                 { id: 2, message: 'Hi!' },
-                { id: 3, message: 'Hi there!' }]
+                { id: 3, message: 'Hi there!' }],
+
+            newMessageBody: '',
         }
     },
     _callSubscriber() { },
@@ -44,25 +46,37 @@ let store = {
         } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
+        } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+            this._state.messagesPage.newMessageBody = action.body;
+            this._callSubscriber(this._state);
+        } else if (action.type === SEND_MESSAGE) {
+            let body = this._state.messagesPage.newMessageBody;
+            this._state.messagesPage.newMessageBody = '';
+            this._state.messagesPage.messagesData.push( { id: 4, message: body } );
+
+            this._callSubscriber(this._state);
         }
     }
 }
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+const SEND_MESSAGE = 'SEND-MESSAGE';
 
 // export const addPostActionCreator = () => ({ type: ADD_POST });
 export const addPostActionCreator = () => {
     return {
         type: ADD_POST
     }
-}
-
+};
 export const updateNewPostTextActionCreator = (text) => {
     return {
         type: UPDATE_NEW_POST_TEXT, newText: text
     }
-}
+};
+export const sendMessageCreator = () => ( {type: SEND_MESSAGE} );
+export const updateNewMessageBodyCreator = (body) => ( {type: UPDATE_NEW_MESSAGE_BODY, body: body} );
 
 window.store = store;
 
