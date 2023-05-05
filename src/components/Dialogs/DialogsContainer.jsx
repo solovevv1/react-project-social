1a9d075ee312d1159152cb563fe37067e745c955
solovevv1 <solovevvs1@gmail.com>
2023-05-05 17:13:55 +0530

    context API

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/message-reducer';
+import StoreContext from '../../StoreContext';
 import DialogItem from './DialogItem/DialogItem';
 import Dialogs from './Dialogs';
 import s from './Dialogs.module.css';
 import Message from './Message/Message';
 
 
-const DialogsContainer = (props) => {
+const DialogsContainer = () => {
 
-    let state = props.store.getState().messagesPage;
 
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator());
-    };
-    let onNewMessageChange = (body) => {
-        props.store.dispatch(updateNewMessageBodyCreator(body));
-    };
 
     return (
-        <Dialogs updateNewMessageBody={onNewMessageChange} 
-                 sendMessage={onSendMessageClick}
-                 messagesPage={state} />
+        <StoreContext.Consumer> 
+            { store => {
+                let state = store.getState().messagesPage;
+
+                let onSendMessageClick = () => {
+                    store.dispatch(sendMessageCreator());
+                };
+                let onNewMessageChange = (body) => {
+                    store.dispatch(updateNewMessageBodyCreator(body));
+                };
+
+                return <Dialogs
+                    updateNewMessageBody={onNewMessageChange}
+                    sendMessage={onSendMessageClick}
+                    messagesPage={state} />
+            }
+        }
+        </StoreContext.Consumer>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/redux-store';
+import StoreContext from './StoreContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 let rerenderEntireTree = (state) => {
   root.render(
     <BrowserRouter>
-      <React.StrictMode>
-        <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
-      </React.StrictMode>
+      <StoreContext.Provider value={store}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </StoreContext.Provider>
     </BrowserRouter>
   );
 }
@@ -22,4 +25,4 @@ rerenderEntireTree(store.getState());
 store.subscribe(() => {
   let state = store.getState();
   rerenderEntireTree(state);
- });
+});
