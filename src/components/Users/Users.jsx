128570ca166b239ffd59ecad12dc689786afb536
solovevv1 <solovevvs1@gmail.com>
2023-05-08 17:51:27 +0530

    rewriting mapDispatchToProps

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,8 +6,8 @@ let Users = (props) => {
 
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
     let pages = [];
-    for (let i = 1; i <= /* pagesCount; */ 20; i++) {
-        pages.push(i + ' ');
+    for (let i = 1; i <= /* pagesCount */20; i++) {
+        pages.push(i);
     }
 
     return <>
