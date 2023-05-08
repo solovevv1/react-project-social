991a1bb103b1d7ecccbe3742a77ed9e870818ee9
solovevv1 <solovevvs1@gmail.com>
2023-05-08 16:30:34 +0530

    preloader, isFetching

---
+++ src/components/Common/Preloader/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import preloader from '../../../assets/images/Ghost.gif'
+
+
+let Preloader = (props) => {
+    return <>
+    <img src={preloader} />
+    </>
+}
+
+export default Preloader;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,15 +6,15 @@ let Users = (props) => {
 
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
     let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
+    for (let i = 1; i <= /* pagesCount; */ 20; i++) {
+        pages.push(i + ' ');
     }
 
     return <>
         <div>
             {pages.map(p => {
                 return <span className={props.currentPage === p && s.selectedPage}
-                    onClick={() => { props.onPageChanged(p) }}>{p}</span>
+                    onClick={ () => {props.onPageChanged(p)} }>{p}</span>
             })}
         </div>
         {
